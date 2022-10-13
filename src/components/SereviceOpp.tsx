e0dac31a736af1197d18ad8fa4937c3dfaed8b34
AlexanderHOtt <aott01@protonmail.ch>
2022-10-12 23:03:17 -0700

    more work on service cards

--- src/components/SereviceOpp.tsx
+++ src/components/SereviceOpp.tsx
@@ -1,27 +1,69 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-type Opp = {
-  name: string
-  desc: string
-  start: number
-  end: number
-  lat: number
-  lon: number
-  id: number
-}
-export const ServiceOpp = (opp: Opp) => {
+export type Opp = {
+  id: string;
+  name: string;
+  desc: string;
+  isChurch: boolean;
+  contact: string;
+  website: string;
+
+  lat: number;
+  lon: number;
+  start: number;
+  end: number;
+};
+
+export const ServiceCard: React.FC<{ opp: Opp }> = ({ opp }) => {
   return (
-    <article>
-      <h3>{opp.name}</h3>
-      <p>{opp.desc}</p>
+    <>
+      <article className="max-w-sm overflow-hidden rounded shadow-lg">
+        <div className="px-6 py-4">
+          <div className="mb-2 text-xl font-bold">{opp.name}</div>
+          <p className="text-base text-gray-700">{opp.desc}</p>
+          <Link href={`https://maps.google.com/maps/@${opp.lat},${opp.lon}`}>
+            <a target="_blank">Google Maps</a>
+          </Link>
+          <p>
+            {new Date(opp.start).toLocaleDateString("en-us", {
+              weekday: "long",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </p>
+          <p>
+            {new Date(opp.end).toLocaleDateString("en-us", {
+              weekday: "long",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </p>
+          <p>{opp.contact}</p>
+          <Link href={opp.website}>Website</Link>
 
-      <div>
-        {new Date(opp.start)} | {new Date(opp.end)}
-      </div>
-
-      <Link href={`https://maps.google.com/@${opp.lat},${opp.lon}`}>
-        Google Maps
-      </Link>
-    </article>
-  )
-}
+          <span
+            className={
+              "mr-2 mb-2 inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 " +
+              (opp.isChurch ? "bg-blue-200" : "bg-green-200")
+            }
+          >
+            {opp.isChurch ? "Church" : "Organization"}
+          </span>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+            #photography
+          </span>
+          <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+            #travel
+          </span>
+          <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+            #winter
+          </span>
+        </div>
+      </article>
+    </>
+  );
+};
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,33 +1,58 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { GetServerSideProps, NextPage } from 'next'
-import { Session, unstable_getServerSession } from 'next-auth'
-import { getSession, useSession } from 'next-auth/react'
-import Link from 'next/link'
-import React, { useMemo, useState } from 'react'
-import { InterestModal, Interests } from '../components/InterestsModal'
-import { env } from '../env/client.mjs'
-import { env as serverEnv } from '../env/server.mjs'
-import { authOptions } from './api/auth/[...nextauth]'
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { GetServerSideProps, NextPage } from "next";
+import { Session, unstable_getServerSession } from "next-auth";
+import { getSession, useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { useMemo, useState } from "react";
+import { InterestModal, Interests } from "../components/InterestsModal";
+import { ServiceCard, type Opp } from "../components/SereviceOpp";
+import { env } from "../env/client.mjs";
+import { env as serverEnv } from "../env/server.mjs";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 export const DashboardPage: NextPage = () => {
+  const { data: session } = useSession();
+  if (!session) {
+    return <div>Please Login</div>;
+  }
 
-  return <div />
-  
-}
+  const {
+    isLoading,
+    error,
+    data: userOpps,
+  } = useQuery<Opp[], Error>(["opps"], async (): Promise<Opp[]> => {
+    const res = await fetch(
+      env.NEXT_PUBLIC_API_URL + `/${session.user?.id}/opps`
+    );
+    const arr = (await res.json()) as Opp[];
+    return arr;
+  });
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        {userOpps?.map((opp) => (
+          <ServiceCard opp={opp} key={opp.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await unstable_getServerSession(
     context.req,
     context.res,
     authOptions
-  )
+  );
 
-  let interests: Interests | null = null
+  let interests: Interests | null = null;
   try {
-    const res = await fetch(serverEnv.API_URL + `/users/${session?.user?.id}`)
-    interests = (await res.json()) as Interests
+    const res = await fetch(serverEnv.API_URL + `/users/${session?.user?.id}`);
+    interests = (await res.json()) as Interests;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-  return { props: { session, interests } }
-}
+  return { props: { session, interests } };
+};
